8af1d3938e3ec1663a83f19f87976d22fcdb8e75
AYandOJ01 <129119876+AYandOJ01@users.noreply.github.com>
2023-07-26 03:41:10 -0700

    update models.py

--- ecommence/Models.py
+++ ecommence/Models.py
@@ -1,22 +1,21 @@
-# ecommerce/models.py
-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class Product(models.Model):
-    name = models.CharField(max_length=100)
+class Products (models.Model):
+    name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=7,decimal_places=3)
     quantity = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
 
-class CustomUser(AbstractUser):
+class CustomerUser(AbstractUser):
+    user_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20)
-    user_validation = models.BooleanField(default=False)
+    validation = models.BooleanField(default=False)
     user_validation_code = models.CharField(max_length=6, blank=True, null=True)
 
     def __str__(self):
-        return self.username
+        return self.user_name
